28a184abc499aa2bba6faa388690d1f3fd0a5479
Enrique Gil Piqueras <enrgil-p@c1r10s2.42madrid.com>
2025-07-24 11:12:34 +0200

    Created general.h. Added libraries

---
+++ general.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   general.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/24 11:05:34 by enrgil-p          #+#    #+#             */
+/*   Updated: 2025/07/24 11:11:38 by enrgil-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GENERAL_H
+# define GENERAL_H
+
+# include <unistd.h>//write, usleep
+# include <stdio.h>//printf
+# include <stdlib.h>//malloc, free
+# include <string.h>//memset
+# include <pthread.h>
+# include <sys/time.h>//gettiimeofday
+
+#endif
