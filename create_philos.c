2cac02b0ff311dab177b51337588ee36614c9479
Enrique Gil Piqueras <enrgil-p@c1r7s1.42madrid.com>
2025-12-16 20:14:58 +0100

    Just created a file for create philos, needs to be designed yet

--- create_philos.c
+++ create_philos.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 19:59:00 by enrgil-p          #+#    #+#             */
-/*   Updated: 2025/12/15 20:44:53 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2025/12/16 20:12:58 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,14 @@
 
 /*void?*/	create_philos(unsigned int *data)
 {
+	int	philos_counter;
+
+	philos_counter = 0;
+	while (++philos_counter <= data[NUM_PHILOS])
+	{
+		//Create a thread, and a fork too
+		//	I guess this means: struct with times and create mutex
+		//	Remember, pmendez talked about five mutex, and people
+		//	use to create those five
+	}
 }
