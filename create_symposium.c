fbbc7baead33a483c5ebec97a61335f767064a45
Enrique Gil Piqueras <enrgil-p@c1r4s1.42madrid.com>
2026-01-17 20:57:01 +0100

    Working in creation of symposium

--- create_symposium.c
+++ create_symposium.c
@@ -1,19 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   create_philos.c                                    :+:      :+:    :+:   */
+/*   create_symposium.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 19:59:00 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/01/17 20:35:09 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/01/17 20:56:19 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "general.h"
 
-//pmendez created philos direclty with a start_routine function that was 
-//the whole states and actions expected from philosophers
+static void	add_parse_data(t_symposium *data, unsigned int *parse_data)
+{
+	data->num_philos = parse_data[NUM_PHILOS];
+	data->die_time = parse_data[TIME_DIE];
+	data->eat_time = parse_data[TIME_EAT];
+	data->sleep_time = parse_data[TIME_SLEEP];
+	data->eat_min_times = parse_data[EAT_MIN_TIMES];
+}
 
 int	create_symposium(unsigned int *data, unsigned int num_philos, t_symposium *roundtable)
 /*t_symposium	create_symposium(unsigned int *data, unsigned int num_philos)*/
@@ -22,13 +28,16 @@ int	create_symposium(unsigned int *data, unsigned int num_philos, t_symposium *r
 	t_philo		philos[num_philos];//Point this with
 					   //roundtable->philos_array
 
-	add_parse_data(data);
+	add_parse_data(roundtable, data);
+	//init_symposium_mutex(roundtable);
+	roundtable->threads_ready = 0;
+	//pthread_create(&roundtable->delphi_oracle, NULL, );
 	counter = 0;
-	while (counter < data[NUM_PHILOS])
+	while (counter < num_philos)
 	{
 		//Where I do create the mutex?
 		//	In struct
-		philos[counter] = add_data(data);
+		philos[counter] = create_a_philo(data);
 		++counter;
 		//Array of structs?
 		//	One general, t_philo in an arry, yes
