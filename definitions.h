f9c65758437a547eba207de18615ec1a85f877dc
Enrique Gil Piqueras <enrgil-p@c1r4s1.42madrid.com>
2026-01-17 18:21:28 +0100

    definitions. Creted messages to print with printf in any state change

--- definitions.h
+++ definitions.h
@@ -6,18 +6,27 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 11:30:56 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/01/08 20:38:34 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/01/17 18:19:56 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFINITIONS_H
 # define DEFINITIONS_H
 
+//Errors
 # define EXPECTED_USAGE_1 "Error. Expected usage: ./philo n_philos time_to_die "
 # define EXPECTED_USAGE_2 "time_to_eat time_to_sleep [n_meals for each philo]\n"
 # define WRONG_ARG "Error. Some argument can't be converted to unsigned int\n"
 # define ZERO_PHILOS "Error. n_philos's value can't be 0\n"
+//
 # define MAX_ARGS 5
+//
+//Logs (messages for 'any state change of a philosopher')
+# define TAKE_FORK "%u %d has taken a fork\n" 
+# define EAT "%u %d is eating\n"
+# define SLEEP "%u %d is sleeping\n"
+# define THINK "%u %d is thinking\n"
+# define DIE "%u %d died\n"
 //
 /*INDEX FOR PARSE_DATA*/
 
