195b183b650182f4908d0ca1f13f14c0e401d4ca
Enrique Gil Piqueras <enrgil-p@c1r4s1.42madrid.com>
2026-02-01 17:43:40 +0100

    Designing time values in definitions.h

--- definitions.h
+++ definitions.h
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 11:30:56 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/01/18 16:52:36 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/02/01 17:43:05 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,24 +45,27 @@ typedef enum e_mutex_index
 	SLEEP,
 	EAT_MIN_TIMES,
 	MAX_MUTEX
-}	t_mutex_index
+}	t_mutex_index;
 
+//Time values are ms, miliseconds. 1 sec == 1.000 ms. 
+//ms will be stored in unsigned long long
 typedef struct s_symposium
 {
-	unsigned int	num_philos;
-	unsigned int	die_time;
-	unsigned int	eat_time;
-	unsigned int	sleep_time;
-	unsigned int	eat_min_times;
-	pthread_mutex_t	mutex[MAX_MUTEX];//Can I use an array?
+	unsigned int		num_philos;
+	unsigned long long	start;
+	unsigned long long	die_time;
+	unsigned long long	eat_time;
+	unsigned long long	sleep_time;
+	unsigned int		eat_min_times;
+	pthread_mutex_t		mutex[MAX_MUTEX];//Can I use an array?
 		/*pthread_mutex_t	init_mutex;
 		pthread_mutex_t	eat_mutex;
 		pthread_mutex_t	sleep_mutex;
 		pthread_mutex_t	die_mutex;
 		pthread_mutex_t	print_mutex;*/
-	int				threads_ready;
+	int					threads_ready;
 	pthread_t		delphi_oracle;//Observer
-	struct s_philo	*philos_array;
+	struct s_philo		*philos_array;
 }	t_symposium;
 
 //This struct is for every philo. All of them, stored in philos_array, above
@@ -71,6 +74,7 @@ typedef struct s_philo
 	int				id;//Integer or unsigned?
 	//type??		life_timer;
 	unsigned int			eaten_times;
+	unsigned long long		last_meal;
 	pthread_t		thread;
 	pthread_mutex_t	fork;
 }	t_philo;
