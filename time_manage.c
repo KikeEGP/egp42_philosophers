113ddb318a47da7e3fead3cacee43cb36c93f947
Enrique Gil Piqueras <enrgil-p@c1r4s1.42madrid.com>
2026-02-01 18:02:20 +0100

    Created functions to manage time

--- time_manage.c
+++ time_manage.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 17:44:34 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/02/01 17:48:00 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/02/01 18:01:39 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,25 @@
 //1 sec == 1000 ms. tv_Sec == seconds, signed integer
 //tv_usec == µs, microsecond, signed integer 
 //	from -1 to 1.000.000 (value of 1 sec). 1 ms == 1000 µs
-int	get_time(unsigned long long* time_in_ms)
+int	get_time(unsigned long long *time_in_ms)
 {
 	struct timeval		unix_time;
-	
-	time_in_ms = 0;
+
+	*time_in_ms = 0;
 	if (gettimeofday(&unix_time, NULL) == -1)
 		return (0);
-	time_in_ms = (unix_time.tv_sec * 1000) + (unix_time.tv_usec / 1000);
+	*time_in_ms = (unix_time.tv_sec * 1000) + (unix_time.tv_usec / 1000);
 	return (1);
 }
 
-//What arpsao- said to isromero
-/*
-unsigned long long start = get_time()
-...
-...
-...
-... bla bla some time
-unsigned long long end = get_time()
-unsigned long long time_passed = end - start;*/
+int	check_time(unsigned long long last_meal,
+		unsigned long long time_to_check)
+{
+	unsigned long long	now;
+
+	if (!get_time(&now))
+		return (2);
+	if ((now - last_meal) >= time_to_check)
+		return (0);
+	return (1);
+}
