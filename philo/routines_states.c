a8ac85259d924ed5d090876957483f7a11f23bb5
Enrique Gil Piqueras <enrgil-p@c2r4s3.42madrid.com>
2026-03-01 18:53:05 +0100

    Moved update of meals between update of last_meal and ft_usleep

--- philo/routines_states.c
+++ philo/routines_states.c
@@ -6,7 +6,7 @@
 /*   By: enrgil-p <enrgil-p@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/18 19:36:35 by enrgil-p          #+#    #+#             */
-/*   Updated: 2026/02/27 22:38:24 by enrgil-p         ###   ########.fr       */
+/*   Updated: 2026/03/01 18:52:25 by enrgil-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,15 +47,15 @@ int	eat_state(t_symposium *table, t_philo *philo)
 	get_unix_time(&philo->last_meal);
 	pthread_mutex_unlock(&table->symp_mutex[CONTROL]);
 	state_change_log(EAT, philo, table, NON_STOP);
+	pthread_mutex_lock(&table->symp_mutex[CONTROL]);
+	if (table->checklist)
+		philo->eaten_times += 1;
+	pthread_mutex_unlock(&table->symp_mutex[CONTROL]);
 	if (!ft_usleep(table->eat_time, table))
 	{
 		release_forks(table, philo);
 		return (0);
 	}
-	pthread_mutex_lock(&table->symp_mutex[CONTROL]);
-	if (table->checklist)
-		philo->eaten_times += 1;
-	pthread_mutex_unlock(&table->symp_mutex[CONTROL]);
 	return (1);
 }
 
@@ -68,14 +68,9 @@ int	sleep_state(t_symposium *table, t_philo *philo)
 	return (1);
 }
 
-//In order to be sure that every philo eats more or less the same times,
-//	think is great scope to put some delay to odd philos
-//	if num_philos is odd too
 void	think_state(t_symposium *table, t_philo *philo)
 {
 	state_change_log(THINK, philo, table, NON_STOP);
-/*	if (table->num_philos % 2 != 0 && philo->id % 2 != 0)
-		usleep(1000);*///Commented by me
 }
 
 void	die_state(t_symposium *table, t_philo *philo)
